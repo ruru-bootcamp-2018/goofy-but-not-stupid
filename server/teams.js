478f7110e7d98c13c29d645cdd10f73ff355d82c
reubenjh <rjharcourt@gmail.com>
2018-08-02 16:29:52 +1200

    fixed poki api ability calls to just do it once per person and reset on new person click

--- server/teams.js
+++ server/teams.js
@@ -1,7 +1,6 @@
 const db = require('./db')
 const shuffle = require('shuffle-array')
 
-
 function makeTeams(cohort, teams) {
   return db.getRelationships()
     .then((relationships) => {
@@ -9,7 +8,6 @@ function makeTeams(cohort, teams) {
       for (let i = 0; i <= 100; i++) {
         possibleTeams.push(makePossibleTeam(cohort, teams.map(o => Object.assign({}, o, {team: []})), relationships))
       }
-
       return possibleTeams
         .sort((a, b) => a.fairness-b.fairness)
         .slice(0, 20)
@@ -17,12 +15,10 @@ function makeTeams(cohort, teams) {
     })
 }
 
-
 function makePossibleTeam(cohort, teams, relationships) {
   shuffle(cohort)
   let targetTeamIndex = 0
   let tempTeams = teams 
-
   cohort.forEach(user => {
     let userSorted = false
     while (!userSorted) {
@@ -46,7 +42,6 @@ function makePossibleTeam(cohort, teams, relationships) {
   return possibleTeam
 }
 
-
 function isTeamFull(team) {
   return team.team.length == team.max
 }
@@ -64,92 +59,5 @@ function addPairingCounts(teams, relationships) {
     }
   })
 }
-
-
-
-
-// function processRelationships(teamSize) {
-//  return db.getRelationships().then(relationships => {
-//     const lowestCount = relationships.reduce((acc, x) => {
-//       if (x.count < acc) acc = x.count
-//       return acc
-//     }, relationships[0].count)
-//     const highestCount = relationships.reduce((acc, x) => {
-//       if (x.count > acc) acc = x.count
-//       return acc
-//     }, relationships[0].count)
-//     let orderedArray = []
-//     for (let i = lowestCount; i <= highestCount; i++) {
-//       const result = relationships.filter(x => x.count == i)
-//       orderedArray.push(result)
-//     }
-//     orderedArray = orderedArray.concat.apply([], orderedArray)
-//     const teams = makeTeams(teamSize)
-
-//     return populateTeams(orderedArray, teams)
-//   })
-// }
-
-// function makeTeams(teamSize) {
-//   for (var teams=[]; teams.push([])<teamSize;){
-//   }
-//   return teams
-// }
-
-// function populateTeams(orderedArray, teams) {
-//   let uniqueIds = getUniqueIds(orderedArray)
-//   let maxSize = Math.ceil(uniqueIds.length / teams.length)
-//   let noNoPairs = theyWhoMustNotBePaired(orderedArray)
-
-//   orderedArray.forEach((pair) => {
-//     let pairIndexes = getIndexes(pair, teams)
-//     if (pairIndexes[0] + pairIndexes[1] == -2){
-//       teams[getEmptiest(teams)].push(pair.id_one, pair.id_two)
-//     } else if (pairIndexes[0] == -1 || pairIndexes[1] == -1){
-//       let sorted = pairIndexes[0] == -1 ? 1 : 0
-//       let currentTeam = teams[pairIndexes[sorted]]
-//       let badPair = neverPair(pair, currentTeam, noNoPairs)
-//       let nextTeam = (currentTeam.length < maxSize && !badPair ? currentTeam : teams[getEmptiest(teams)])
-//       nextTeam.push(sorted == 0 ? pair.id_two : pair.id_one)
-//     }
-//   })
-//   return teams
-// }
-
-// function theyWhoMustNotBePaired(pairs) {
-//   let maxCount = pairs[pairs.length - 1].count
-//   return pairs.filter((pair) => pair.count == maxCount)
-// }
-
-// function neverPair(pair, team, noNoPairs){
-//   let newPerson = team.includes(pair.id_one) ? pair.id_two : pair.id_one
-//   return noNoPairs.find((badPair) => {
-//     return (badPair.id_one == newPerson && team.includes(badPair.id_two)) || (badPair.id_two == newPerson && team.includes(badPair.id_one))
-//   })
-// }
-
-// function getUniqueIds(pairsArr) {
-//   let arr = [];
-//   pairsArr.forEach((pair) => {
-//     if(!arr.includes(pair.id_one)) arr.push(pair.id_one)
-//     if(!arr.includes(pair.id_two)) arr.push(pair.id_two)
-//   })
-//   return arr
-// }
-
-// function getIndexes(pair, teams) {
-//   let arr = [-1, -1]
-//   teams.forEach((team, i) => {
-//     if(team.includes(pair.id_one)) arr[0] = i
-//     if(team.includes(pair.id_two)) arr[1] = i
-//   })
-//   return arr
-// }
-
-// function getEmptiest(teams) {
-//   lengthsArr = teams.map((team) => team.length)
-//   return lengthsArr.indexOf(Math.min.apply(Math, lengthsArr))
-// }
-
 
 module.exports = makeTeams
