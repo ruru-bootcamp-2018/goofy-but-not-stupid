9683c45944b1989d873ad5d008c7e8c1e78e2da7
reubenjh <rjharcourt@gmail.com>
2018-08-02 12:49:59 +1200

    random teamsorting works - need to check pairing counts next

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,39 +1,52 @@
 const express = require('express')
 const db = require('../db')
-const teams = require('../teams')
+const makeTeams = require('../teams')
 const api = require('../api')
+
+const tempTeamSelection = [
+  {
+    max: 5,
+    team: [],
+    full: false
+  },
+  {
+    max: 5,
+    team: [],
+    full: false
+  },
+  {
+    max: 5,
+    team: [],
+    full: false
+  }
+]
 
 const router = express.Router()
 
-// const cohort = {
-//   1: 'Dan',
-//   2: 'Rebecca D',
-//   3: 'Riki',
-//   4: 'Reuben',
-//   5: 'Brad',
-//   6: 'Cate',
-//   7: 'Tay',
-//   8: 'Rebecca L',
-//   9: 'Ross',
-//   10: 'Stan',
-//   11: 'Anton',
-//   12: 'Hayden',
-//   13: 'Kelly',
-//   14: 'Phoenix',
-//   15: 'Cliff'
-// }
-
 router.get('/team', (req, res) => {
-  teams.processRelationships(3)
-  .then(team => {
-    res.json(team)
-  })
+  let teams = [...tempTeamSelection] // will switch this route to post with the teamselection
+  db.getUsers()
+    .then(cohort => {
+      new Promise((resolve, reject) => {
+        makeTeams(cohort, teams, resolve)
+      })
+      .then(finalTeam => {
+        console.log(finalTeams)
+      })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+
+  // teams.processRelationships(3)
+  // .then(team => {
+  //   res.json(team)
+  // })
 })
 
 router.get('/', (req, res) => {
   db.getUsers()
     .then(users => {
-      // console.log(users)
       res.json({users})
     })
 })
@@ -41,13 +54,11 @@ router.get('/', (req, res) => {
 router.get('/poki', (req, res) => {
   api.getPoki()
     .then((poki) => {
-      // console.log(poki)
       res.json(poki)
     })
 })
 
 router.get('/:name', (req, res) => {
-  console.log('made it to server side')
   let name = req.params.name
   db.getUserData(name)
     .then(userData => {
--- server/teams.js
+++ server/teams.js
@@ -1,91 +1,133 @@
 const db = require('./db')
+const shuffle = require('shuffle-array')
 
-function processRelationships(teamSize) {
- return db.getRelationships().then(relationships => {
-    const lowestCount = relationships.reduce((acc, x) => {
-      if (x.count < acc) acc = x.count
-      return acc
-    }, relationships[0].count)
-    const highestCount = relationships.reduce((acc, x) => {
-      if (x.count > acc) acc = x.count
-      return acc
-    }, relationships[0].count)
-    let orderedArray = []
-    for (let i = lowestCount; i <= highestCount; i++) {
-      const result = relationships.filter(x => x.count == i)
-      orderedArray.push(result)
-    }
-    orderedArray = orderedArray.concat.apply([], orderedArray)
-    const teams = makeTeams(teamSize)
 
-    return populateTeams(orderedArray, teams)
-  })
-}
+function makeTeams(cohort, teams, resolve) {
+  shuffle(cohort)
+  let teamCount = teams.length
+  let targetTeamIndex = 0
+  let tempTeams = [...teams]
 
-function makeTeams(teamSize) {
-  for (var teams=[]; teams.push([])<teamSize;){
-  }
-  return teams
-}
-
-function populateTeams(orderedArray, teams) {
-  let uniqueIds = getUniqueIds(orderedArray)
-  let maxSize = Math.ceil(uniqueIds.length / teams.length)
-  let noNoPairs = theyWhoMustNotBePaired(orderedArray)
-
-  orderedArray.forEach((pair) => {
-    let pairIndexes = getIndexes(pair, teams)
-    if (pairIndexes[0] + pairIndexes[1] == -2){
-      teams[getEmptiest(teams)].push(pair.id_one, pair.id_two)
-    } else if (pairIndexes[0] == -1 || pairIndexes[1] == -1){
-      let sorted = pairIndexes[0] == -1 ? 1 : 0
-      let currentTeam = teams[pairIndexes[sorted]]
-      let badPair = neverPair(pair, currentTeam, noNoPairs)
-      let nextTeam = (currentTeam.length < maxSize && !badPair ? currentTeam : teams[getEmptiest(teams)])
-      nextTeam.push(sorted == 0 ? pair.id_two : pair.id_one)
+  cohort.forEach(user => {
+    console.log(`sorting ${user.name}`)
+    let userSorted = false
+    while (!userSorted) {
+      if (isTeamFull(tempTeams[targetTeamIndex])) {
+        console.log(targetTeamIndex)
+        targetTeamIndex == tempTeams.length-1 ? targetTeamIndex = 0 : targetTeamIndex++
+        console.log(targetTeamIndex)
+      } else {
+        tempTeams[targetTeamIndex].team.push(user)
+        tempTeams[targetTeamIndex].team.length < tempTeams[targetTeamIndex].max ? tempTeams[targetTeamIndex].full = false : tempTeams[targetTeamIndex].full = true
+        targetTeamIndex == tempTeams.length-1 ? targetTeamIndex = 0 : targetTeamIndex++
+        userSorted = true
+      } 
     }
   })
-  return teams
+
+  // checkPairingCounts(tempTeams)
 }
 
-function theyWhoMustNotBePaired(pairs) {
-  let maxCount = pairs[pairs.length - 1].count
-  return pairs.filter((pair) => pair.count == maxCount)
+function isTeamFull(team) {
+  return team.full
 }
 
-function neverPair(pair, team, noNoPairs){
-  let newPerson = team.includes(pair.id_one) ? pair.id_two : pair.id_one
-  return noNoPairs.find((badPair) => {
-    return (badPair.id_one == newPerson && team.includes(badPair.id_two)) || (badPair.id_two == newPerson && team.includes(badPair.id_one))
-  })
-}
-
-function getUniqueIds(pairsArr) {
-  let arr = [];
-  pairsArr.forEach((pair) => {
-    if(!arr.includes(pair.id_one)) arr.push(pair.id_one)
-    if(!arr.includes(pair.id_two)) arr.push(pair.id_two)
-  })
-  return arr
-}
-
-function getIndexes(pair, teams) {
-  let arr = [-1, -1]
-  teams.forEach((team, i) => {
-    if(team.includes(pair.id_one)) arr[0] = i
-    if(team.includes(pair.id_two)) arr[1] = i
-  })
-  return arr
-}
-
-function getEmptiest(teams) {
-  lengthsArr = teams.map((team) => team.length)
-  return lengthsArr.indexOf(Math.min.apply(Math, lengthsArr))
+function checkPairingCounts (teams) {
+  db.getRelationships()
+    .then((relationships) => {
+      teams.forEach((team) => {
+        team.team.forEach(member => {
+          // hmmmm
+        })
+      })
+    })
 }
 
 
-module.exports = {
-  processRelationships,
-  makeTeams,
-  populateTeams
-}
+
+
+// function processRelationships(teamSize) {
+//  return db.getRelationships().then(relationships => {
+//     const lowestCount = relationships.reduce((acc, x) => {
+//       if (x.count < acc) acc = x.count
+//       return acc
+//     }, relationships[0].count)
+//     const highestCount = relationships.reduce((acc, x) => {
+//       if (x.count > acc) acc = x.count
+//       return acc
+//     }, relationships[0].count)
+//     let orderedArray = []
+//     for (let i = lowestCount; i <= highestCount; i++) {
+//       const result = relationships.filter(x => x.count == i)
+//       orderedArray.push(result)
+//     }
+//     orderedArray = orderedArray.concat.apply([], orderedArray)
+//     const teams = makeTeams(teamSize)
+
+//     return populateTeams(orderedArray, teams)
+//   })
+// }
+
+// function makeTeams(teamSize) {
+//   for (var teams=[]; teams.push([])<teamSize;){
+//   }
+//   return teams
+// }
+
+// function populateTeams(orderedArray, teams) {
+//   let uniqueIds = getUniqueIds(orderedArray)
+//   let maxSize = Math.ceil(uniqueIds.length / teams.length)
+//   let noNoPairs = theyWhoMustNotBePaired(orderedArray)
+
+//   orderedArray.forEach((pair) => {
+//     let pairIndexes = getIndexes(pair, teams)
+//     if (pairIndexes[0] + pairIndexes[1] == -2){
+//       teams[getEmptiest(teams)].push(pair.id_one, pair.id_two)
+//     } else if (pairIndexes[0] == -1 || pairIndexes[1] == -1){
+//       let sorted = pairIndexes[0] == -1 ? 1 : 0
+//       let currentTeam = teams[pairIndexes[sorted]]
+//       let badPair = neverPair(pair, currentTeam, noNoPairs)
+//       let nextTeam = (currentTeam.length < maxSize && !badPair ? currentTeam : teams[getEmptiest(teams)])
+//       nextTeam.push(sorted == 0 ? pair.id_two : pair.id_one)
+//     }
+//   })
+//   return teams
+// }
+
+// function theyWhoMustNotBePaired(pairs) {
+//   let maxCount = pairs[pairs.length - 1].count
+//   return pairs.filter((pair) => pair.count == maxCount)
+// }
+
+// function neverPair(pair, team, noNoPairs){
+//   let newPerson = team.includes(pair.id_one) ? pair.id_two : pair.id_one
+//   return noNoPairs.find((badPair) => {
+//     return (badPair.id_one == newPerson && team.includes(badPair.id_two)) || (badPair.id_two == newPerson && team.includes(badPair.id_one))
+//   })
+// }
+
+// function getUniqueIds(pairsArr) {
+//   let arr = [];
+//   pairsArr.forEach((pair) => {
+//     if(!arr.includes(pair.id_one)) arr.push(pair.id_one)
+//     if(!arr.includes(pair.id_two)) arr.push(pair.id_two)
+//   })
+//   return arr
+// }
+
+// function getIndexes(pair, teams) {
+//   let arr = [-1, -1]
+//   teams.forEach((team, i) => {
+//     if(team.includes(pair.id_one)) arr[0] = i
+//     if(team.includes(pair.id_two)) arr[1] = i
+//   })
+//   return arr
+// }
+
+// function getEmptiest(teams) {
+//   lengthsArr = teams.map((team) => team.length)
+//   return lengthsArr.indexOf(Math.min.apply(Math, lengthsArr))
+// }
+
+
+module.exports = makeTeams
