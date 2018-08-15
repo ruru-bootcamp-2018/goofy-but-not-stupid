0a15c3be98883efbb98c5839bc4ba1addf7d5b5a
reubenjh <rjharcourt@gmail.com>
2018-08-16 07:39:04 +1200

    converted app.jsx to functional component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {HashRouter as Router, Route} from 'react-router-dom'
+import { HashRouter as Router, Route } from 'react-router-dom'
 
 import Home from './Home'
 import HomeTeams from './HomeTeams'
@@ -7,31 +7,18 @@ import Login from './Login'
 import Register from './Register'
 import Nav from './Nav'
 
-///
-
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-    }
-
-  }
-
-
-  render()  {
-    return (
-      <Router>
-        <div className='app'>
+const App = () => {
+  return (
+    <Router>
+      <div className='app'>
         <Nav />
         <Route exact path='/' component={Home} />
         <Route exact path='/teams' component={HomeTeams} />
         <Route exact path='/login' component={Login} />
         <Route exact path='/register' component={Register} />
-        </div>
-      </Router>
-    )
-  }
+      </div>
+    </Router>
+  )
 }
-
 
 export default App
