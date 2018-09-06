915f969c8135c77e1fa3e4cceaab61df51c0f417
Rebecca Dugdale <bec.dugdale16@gmail.com>
2018-09-07 10:42:52 +1200

    display group names

--- client/components/PreviousTeams.jsx
+++ client/components/PreviousTeams.jsx
@@ -12,24 +12,36 @@ class PreviousTeams extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            teams: []
+            groups: []
         }
+      this.findGroup = this.findGroup.bind(this)
     }
 
     componentDidMount() {
+      this.setState({
+        groups: this.props.dispatch(getGroups(this.props.auth.user.id))
+      })
+    }
+
+    findGroup(name) {
 
     }
 
     render() {
+      const groups = this.props.groups.groups
         return (
             <React.Fragment>
               <BackToHomeButton />
-
+                <div className="columns is-multiline">
+                  <ul>
+                  {groups.map(group => <li key={group.name}><a><h4>{group.name}</h4></a></li>)}
+                  </ul>
+                </div>
               <Footer />
             </React.Fragment>
         )
     }
   }
 
-  const mapStateToProps = ({ auth, users }) => ({ auth, users })
+  const mapStateToProps = ({ auth, users, groups }) => ({ auth, users, groups })
   export default connect(mapStateToProps)(PreviousTeams)
